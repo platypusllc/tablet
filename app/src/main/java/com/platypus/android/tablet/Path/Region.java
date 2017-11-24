4536a1b58956f25d46ac739eb662957aa701bfe9
jjblum <jason.j.blum@gmail.com>
2017-11-24 17:53:24 +0100

    Mostly functional closed-hull spiral. Needs extensive testing, but shouldn't explode...

--- app/src/main/java/com/platypus/android/tablet/Path/Region.java
+++ app/src/main/java/com/platypus/android/tablet/Path/Region.java
@@ -10,6 +10,8 @@ import org.jscience.geography.coordinates.crs.ReferenceEllipsoid;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.measure.unit.NonSI;
 import javax.measure.unit.SI;
@@ -24,11 +26,14 @@ public class Region
 		private ArrayList<Double[]> convex_xy = new ArrayList<>();
 		private ArrayList<Double[]> path_xy = new ArrayList<>();
 		private ArrayList<LatLng> path_points = new ArrayList<>();
-		private Double[] centroid = new Double[2];
+		private Double[] utm_centroid = new Double[2];
+		private Double[] local_centroid = new Double[2];
 		private UTM original_utm;
 		private AreaType area_type;
 		private double transect_distance = 10;
 		private String logTag = "Region";
+		private double last_min_dist_to_centroid = 9999999; // start as a huge number
+		private double last_max_dist_to_centroid = 9999999; // start as a huge number
 
 		public Region (ArrayList<LatLng> _original_points, AreaType _area_type, double _transect_distance) throws Exception
 		{
@@ -62,8 +67,8 @@ public class Region
 				for (Double[] p : path_xy)
 				{
 						// add back in the UTM offset
-						p[0] += centroid[0];
-						p[1] += centroid[1];
+						p[0] += utm_centroid[0];
+						p[1] += utm_centroid[1];
 						LatLong latLong = UTM.utmToLatLong(UTM.valueOf(original_utm.longitudeZone(), original_utm.latitudeZone(), p[0], p[1], SI.METER), ReferenceEllipsoid.WGS84);
 						path_points.add(new LatLng(latLong.latitudeValue(NonSI.DEGREE_ANGLE), latLong.longitudeValue(NonSI.DEGREE_ANGLE)));
 				}
@@ -108,7 +113,7 @@ public class Region
 
 		private Double[] difference(Double[] a, Double[] b)
 		{
-				return new Double[]{b[0]-a[0], b[1] - a[1]};
+				return new Double[]{b[0] - a[0], b[1] - a[1]};
 		}
 
 		private double distance(Double[] a, Double[] b)
@@ -123,7 +128,8 @@ public class Region
 
 		private double distance_to_centroid(Double[] a)
 		{
-				return distance(a, centroid);
+				return distance(a, local_centroid);
+				// return Math.sqrt(a[0]*a[0] + a[1]*a[1]); // assuming centroid is 0, 0
 		}
 
 		private double min_distance_to_centroid(ArrayList<Double[]> points)
@@ -135,6 +141,100 @@ public class Region
 						if (min_distance < 0 || d < min_distance) min_distance = d;
 				}
 				return min_distance;
+		}
+
+		private double max_distance_to_centroid(ArrayList<Double[]> points)
+		{
+				double max_distance = -1;
+				for (int i = 0; i < points.size(); i++)
+				{
+						double d = distance_to_centroid(points.get(i));
+						if (max_distance < 0 || d > max_distance) max_distance = d;
+				}
+				return max_distance;
+		}
+
+		private double[][] pairwiseDistances(ArrayList<Double[]> points)
+		{
+				double[][] result = new double[points.size()][points.size()];
+				for (int i = 0; i < points.size(); i++)
+				{
+						for (int j = 0; j < points.size(); j++)
+						{
+								result[i][j] = distance(points.get(i), points.get(j));
+						}
+				}
+				return result;
+		}
+
+		private int[] diameterPair(ArrayList<Double[]> points)
+		{
+				double[][] pairwise_distances = pairwiseDistances(points);
+				double max_dist = -1.;
+				int maxi = 0;
+				int maxj = 0;
+				for (int i = 0; i < points.size(); i++)
+				{
+						for (int j = 0; j < points.size(); j++)
+						{
+								if (pairwise_distances[i][j] > max_dist)
+								{
+										maxi = i;
+										maxj = j;
+										max_dist = pairwise_distances[i][j];
+								}
+						}
+				}
+				return new int[]{maxi, maxj};
+		}
+
+		private ArrayList<Double[]> mergeClosePoints(ArrayList<Double[]> points)
+		{
+				// merge one pair at a time, recurse until no pairs can be merged OR only 3 points remain OR all points would merge
+				if (points.size() < 4) return points;
+				ArrayList<Double[]> result = new ArrayList<>();
+				double[][] pairwise_distances = pairwiseDistances(points);
+				HashMap<Integer, Integer> merging_map = new HashMap<>();
+				for (int i = 0; i < points.size(); i++)
+				{
+						for (int j = 0; j < points.size(); j++)
+						{
+								if ((i != j) && (pairwise_distances[i][j] < transect_distance))
+								{
+										if (!merging_map.containsKey(j)) merging_map.put(i, j);
+										break;
+								}
+						}
+				}
+				if (merging_map.size() == 0) return points;
+				Log.w(logTag, String.format("There are %d points to merge", merging_map.size()));
+
+				// extract only the first merge pair
+				int a = -1; // indices
+				int b = -1;
+				for (Map.Entry<Integer, Integer> entry : merging_map.entrySet())
+				{
+						a = entry.getKey();
+						b = entry.getValue();
+						Log.w(logTag, String.format("Merging index %d and %d", a, b));
+						break;
+				}
+
+				for (int i = 0; i < points.size(); i++)
+				{
+						if (i == a)
+						{
+								// merge these two
+								Double[] pa = points.get(a);
+								Double[] pb = points.get(b);
+								result.add(new Double[]{0.5*pa[0]+0.5*pb[0], 0.5*pa[1]+0.5*pb[1]});
+						}
+						else if (i != b)
+						{
+								result.add(points.get(i));
+						}
+				}
+				return mergeClosePoints(result);
 		}
 
 		private Double[] calculateCentroid(ArrayList<Double[]> points)
@@ -164,6 +264,7 @@ public class Region
 		{
 				ArrayList<Double[]> rolled_points = shiftArrayList(points, 1);
 
+				/*
 				StringBuilder sb0, sb1;
 				sb0 = new StringBuilder();
 				sb1 = new StringBuilder();
@@ -176,13 +277,14 @@ public class Region
 				}
 				Log.v(logTag, "points = \n" + sb0.toString());
 				Log.v(logTag, "rolled_points = \n" + sb1.toString());
+				*/
 
 				ArrayList<Double> result = new ArrayList<>();
 				for (int i = 0; i < points.size(); i++)
 				{
 						Double[] a = rolled_points.get(i);
 						Double[] b = points.get(i);
-						Log.d(logTag, String.format("a = [%.0f, %.0f], b = [%.0f, %.0f]", a[0], a[1], b[0], b[1]));
+						//Log.v(logTag, String.format("a = [%.0f, %.0f], b = [%.0f, %.0f]", a[0], a[1], b[0], b[1]));
 						double raw_angle = Math.atan2(b[1]-a[1], b[0]-a[0]) + Math.PI/2.0;
 						result.add(wrapToPi(raw_angle));
 				}
@@ -205,15 +307,16 @@ public class Region
 		{
 				// recursive. Given the previous set of points, calculate the next set of points. Append them to path_xy until the centroid is reached.
 				// the first points (i.e. path_xy is empty) are the first convex hull
-				// TODO: make sure the final point (i.e. the final recursive call is completed) is the centroid
-
-				// calculate minimum distance to centroid
-
+				// the final point is the centroid
+				Log.d(logTag, String.format("inwardNextHull called. path_xy has %d points", path_xy.size()));
 				if (path_xy.size() < 1)
 				{
 						// first call: just add convex hull to path and recurse
 						path_xy.addAll(previous_hull);
+						path_xy.add(path_xy.get(0).clone());
 						inwardNextHull(previous_hull);
+						last_min_dist_to_centroid = 9999999;
+						last_max_dist_to_centroid = 9999999;
 						return; // recursion stack unravels here, so return, now with path_xy fully populated
 				}
 
@@ -231,15 +334,17 @@ public class Region
 						double rolled_angle = rolled_angles.get(i);
 						Log.v(logTag, String.format("Normal angle: %f", angle));
 						Log.v(logTag, String.format("Rolled normal angle: %f", rolled_angle));
-						new_segments_point_1.add(new Double[]{
+						Double[] new_1 = new Double[]{
 										p[0] + transect_distance*Math.cos(angle),
-										p[1] + transect_distance*Math.sin(angle)});
-						new_segments_point_2.add(new Double[]{
+										p[1] + transect_distance*Math.sin(angle)};
+						Double[] new_2 = new Double[]{
 										p[0] + transect_distance*Math.cos(rolled_angle),
-										p[1] + transect_distance*Math.sin(rolled_angle)});
+										p[1] + transect_distance*Math.sin(rolled_angle)};
+						new_segments_point_1.add(new_1);
+						new_segments_point_2.add(new_2);
 				}
 
-				// shift new_segments_point_2 so that the proper new lines have their indices aligned
+				// shift new_segments_point_2 so that the new lines have their indices aligned properly
 				new_segments_point_2 = shiftArrayList(new_segments_point_2, 1);
 
 				// find intersections between the new lines
@@ -254,9 +359,120 @@ public class Region
 										new_segments_point_1_rolled.get(i),
 										new_segments_point_2_rolled.get(i)));
 				}
-				path_xy.addAll(intersections);
 
-				// TODO: determine if this is the final iteration, if not, recurse using intersections
+				// to compare old to new for overshoot, need to shift back by one (2nd must become 1st)
+				intersections = shiftArrayList(intersections, -1);
+
+				// check for overshoot, ending early if you find it
+				local_centroid = calculateCentroid(intersections);
+				for (int i = 0; i < previous_hull.size(); i++)
+				{
+						Double[] p_old = previous_hull.get(i);
+						Double[] p_new = intersections.get(i);
+						Double[] diff_old = difference(p_old, local_centroid);
+						Double[] diff_new = difference(p_new, local_centroid);
+						double old_coord = 0;
+						double new_coord = 0;
+						String which_coordinate;
+
+						// only use the larger of the two coordinates to avoid false positives
+						if (Math.abs(diff_old[0]) > Math.abs(diff_old[1]))
+						{
+								old_coord = diff_old[0];
+								new_coord = diff_new[0];
+								which_coordinate = "x";
+						}
+						else
+						{
+								old_coord = diff_old[1];
+								new_coord = diff_new[1];
+								which_coordinate = "y";
+						}
+						if (old_coord*new_coord <= 0)
+						{
+								Log.i(logTag, String.format("inwardNextHull detected overshoot: index %d, %s", i, which_coordinate));
+								path_xy.add(local_centroid);
+								last_min_dist_to_centroid = 9999999;
+								last_max_dist_to_centroid = 9999999;
+								return;
+						}
+				}
+
+				// to create a *closed-hull* spiral we need to shift back by one again (3rd must become 1st)
+				intersections = shiftArrayList(intersections, -1);
+
+				// merge any points that are too close together to track
+				intersections = mergeClosePoints(intersections);
+
+				local_centroid = calculateCentroid(intersections);
+				/*
+				double min_d_to_c = min_distance_to_centroid(intersections);
+				double max_d_to_c = max_distance_to_centroid(intersections);
+				if (min_d_to_c <= transect_distance)
+				{
+						Log.i(logTag, "inwardNextHull detected a point < transect_distance from the centroid.");
+						path_xy.add(local_centroid);
+						last_min_dist_to_centroid = 9999999;
+						return;
+				}
+				Log.d(logTag, String.format("old min dist = %.0f, new min dist = %.0f", last_min_dist_to_centroid, min_d_to_c));
+				Log.d(logTag, String.format("old max dist = %.0f, new max dist = %.0f", last_max_dist_to_centroid, max_d_to_c));
+				if (min_d_to_c < (last_min_dist_to_centroid + transect_distance))
+				{
+						// include that extra buffer of + transect_distance. Only trigger when the algorithm is exploding
+						last_min_dist_to_centroid = min_d_to_c;
+				}
+				else
+				{
+						Log.i(logTag, "inwardNextHull detected an increase in min distance to centroid");
+						// reduction in minimum distance should be monotonic
+						path_xy.add(local_centroid);
+						last_min_dist_to_centroid = 9999999;
+						last_max_dist_to_centroid = 9999999;
+						return;
+				}
+				if (max_d_to_c < (last_max_dist_to_centroid + transect_distance))
+				{
+						last_max_dist_to_centroid = max_d_to_c;
+				}
+				else
+				{
+						Log.i(logTag, "inwardNextHull detected an increase in max distance to centroid");
+						// reduction in minimum distance should be monotonic
+						path_xy.add(local_centroid);
+						last_min_dist_to_centroid = 9999999;
+						last_max_dist_to_centroid = 9999999;
+						return;
+				}
+				*/
+
+				// TODO: check that the spiral always turns the same way, point by point.
+				// TODO:     if it ever deviates, truncate with the centroid and end
+				Double[] p0 = path_xy.get(path_xy.size()-2);
+				Double[] p1 = path_xy.get(path_xy.size()-1);
+				double old_angle = Math.atan2(p1[1]-p0[1], p1[0]-p0[0]);
+				for (int i = 0; i < intersections.size(); i++)
+				{
+						Double[] p2 = intersections.get(i);
+						double angle = Math.atan2(p2[1]-p1[1], p2[0]-p1[0]);
+						if (wrapToPi(angle - old_angle) > -Math.PI/8) // angle should usually increase, certainly no sharp drops
+						{
+								path_xy.add(p2);
+								p1 = p2;
+								old_angle = angle;
+						}
+						else
+						{
+								Log.i(logTag, "inwardNextHull detected wrong spiral direction. Truncating");
+								Log.d(logTag, String.format("wrong spiral, old_angle = %.6f, angle = %.6f", old_angle, angle));
+								path_xy.add(local_centroid);
+								last_min_dist_to_centroid = 9999999;
+								last_max_dist_to_centroid = 9999999;
+								return;
+						}
+				}
+				path_xy.add(intersections.get(0).clone()); // close the hull before moving to inward hull
+				inwardNextHull(intersections);
 		}
 
 		private void convexHull() throws Exception
@@ -287,12 +503,13 @@ public class Region
 
 		private ArrayList<Double[]> zeroCentroid(ArrayList<Double[]> points)
 		{
-				centroid = calculateCentroid(points);
+				utm_centroid = calculateCentroid(points);
+				local_centroid = new Double[]{0., 0.};
 				ArrayList<Double[]> result = new ArrayList<>();
 				for (int i = 0; i < points.size(); i++)
 				{
 						Double[] p = points.get(i);
-						result.add(new Double[]{p[0] - centroid[0], p[1] - centroid[1]});
+						result.add(new Double[]{p[0] - utm_centroid[0], p[1] - utm_centroid[1]});
 				}
 				return result;
 		}
