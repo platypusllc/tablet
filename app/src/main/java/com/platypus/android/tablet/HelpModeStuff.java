624bfa0bee4df91cb42e4014309699a91af6a769
jjblum <jason.j.blum@gmail.com>
2018-03-30 12:23:37 +0200

    TourGuide library is now ready for use.

---
+++ app/src/main/java/com/platypus/android/tablet/HelpModeStuff.java
@@ -0,0 +1,12 @@
+package com.platypus.android.tablet;
+
+import tourguide.tourguide.TourGuide;
+
+/**
+ * Created by jason on 3/30/18.
+ */
+
+public class HelpModeStuff
+{
+		TourGuide tourGuide;
+}
